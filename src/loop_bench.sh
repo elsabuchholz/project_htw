edf3e07f23d4405546cd9d27e6a3dd13ddef93fc
Elsa Buchholz <s0544180@htw-berlin.de>
2018-12-07 21:33:58 +0100

    fixed output hang on file

--- src/loop_bench.sh
+++ src/loop_bench.sh
@@ -2,10 +2,11 @@
 COUNTER=1
 while  [ $COUNTER -le 3 ]; do
 make src/bench_toupper
-./out/bench_toupper  > output.txt
+echo "$COUNTER" >> output.txt
+./out/bench_toupper  >> output.txt
 sleep 10s
 rm -rf /lfs/sometext.txt
-< /dev/urandom tr -dc "X" | head -c1000 >> /lfs/sometext.txt
+< /dev/urandom tr -dc "X" | head -c1000 > /lfs/sometext.txt
 echo The counter is $COUNTER
  COUNTER=$((COUNTER+1))
 done
