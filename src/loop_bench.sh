0a6b3005a0967ec9632f6caa2b0250d60206b84d
Elsa Buchholz <s0544180@htw-berlin.de>
2018-12-07 22:21:38 +0100

    test sleep function

--- src/loop_bench.sh
+++ src/loop_bench.sh
@@ -4,10 +4,10 @@ make bench-toupper-formatted
 #counter um das skript x mal auszuführen in while do loop
 COUNTER=1
 while  [ $COUNTER -le 3 ]; do
-echo "$COUNTER" >> 1000.txt
-./out/bench_toupper_formatted  >> 1000.txt
+echo "$COUNTER" >> 1000.csv
+./out/bench_toupper_formatted  >> 1000.csv
 #sleep um sicher zu stellen, dass sometext nicht entfernt wird vor Änderungen
-sleep 10
+sleep 30m
 rm -rf /lfs/sometext.txt
 < /dev/urandom tr -dc "X" | head -c1000 > /lfs/sometext.txt
 echo The counter is $COUNTER
