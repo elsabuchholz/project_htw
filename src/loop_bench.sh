4a0d24df2da8c8deb43ce89dde8d25d4c0b90910
Elsa Buchholz <s0544180@htw-berlin.de>
2018-12-07 21:14:59 +0100

    added counter

--- src/loop_bench.sh
+++ src/loop_bench.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+COUNTER=0
+while [  $COUNTER -lt 10 ]; do
 make src/bench_toupper
 ./out/bench_toupper  > output.txt
 sleep 10s
 rm -rf /lfs/sometext.txt
 < /dev/urandom tr -dc "X" | head -c1000 > /lfs/sometext.txt
+echo The counter is $COUNTER
+let COUNTER=COUNTER+1 
+done
