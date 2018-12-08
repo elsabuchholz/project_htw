296542d735fc1bdfbc5f25317cf1cc9c4b40e295
Elsa Buchholz <s0544180@htw-berlin.de>
2018-12-08 11:47:48 +0100

    added size_tr

--- src/loop_bench.sh
+++ src/loop_bench.sh
@@ -4,15 +4,20 @@
 #size 10K - 20M erst ab 4K kann gemessen werden, da character mehr als 4K groß ist
 #10K
 #size=10000
+#size_tr=10K
 #100K
 #size=100000
+#size_tr=100K
 #1M
-size=1000000
-size_tr=1M
+#size=1000000
+#size_tr=1M
 #10M
 #size=10000000
+#size_tr=10M
 #20M
 #size=20000000
+#size_tr=20M
+
 dd if=/dev/urandom bs=$size count=1 | base64 > /home/l4mdc/sometext.notform &&
 cat /home/l4mdc/sometext.notform | tr 'A-Za-z0-9+/=' 'a-z' > /home/l4mdc/sometext.txt &&
 truncate -s $size_tr /home/l4mdc/sometext.txt
@@ -27,7 +32,7 @@ COUNTER=1
 while  [ $COUNTER -le 100 ]; do
   echo "cp sometext to lfs"
 cp /home/l4mdc/sometext.txt /lfs/sometext.txt &&
-./out/bench_toupper_formatted  >> rslt/$size.csv
+./out/bench_toupper_formatted  >> rslt/$size_tr.csv
 #sleep um sicher zu stellen, dass sometext nicht entfernt wird vor Änderungen
 sleep 10s
 #entfernen der random datei und kopieren der vorher erstellten Datei
