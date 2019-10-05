d7c4a8c48530918709ee748630bc6f0165b0daed
Elsa Buchholz <s0544180@htw-berlin.de>
2019-10-05 14:50:51 +0200

    clean code

--- src/mmap/2_src_dst_example.c
+++ src/mmap/2_src_dst_example.c
@@ -10,19 +10,23 @@ int main(int argc, char **argv){
 
     int sfd, dfd, i;
     char *src, *dest;
-    /*size_t filesize;*/
-    size_t filesize = 10*1024*1024;
+    size_t filesize = 10*1024*1024; //10MB
+
+    /* HELPER FUNCTION TO WRITE ALL STDOUT TO FILE*/
 
     freopen("/home/l4mdc/sometextmem.txt", "a+", stdout);
 
-
     /* SOURCE */
     sfd = open("/home/l4mdc/sometext.txt", O_RDONLY);
+
+    /* OPTION TO WRITE THE SIZE FROM THE GIVEN FILE*/
+    /*size_t filesize;*/
     /*filesize = lseek(sfd, 0, SEEK_END);*/
 
     src = mmap(NULL, filesize, PROT_READ, MAP_PRIVATE, sfd, 0);
 
     /* DESTINATION */
+    
     dfd = open("/lfs/memcpy", O_RDWR | O_CREAT, 0666);
 
     ftruncate(dfd, filesize);
@@ -32,30 +36,16 @@ int main(int argc, char **argv){
     /* COPY */
 
     memcpy(dest, src, filesize);
-    /* Read the file char-by-char from the mmap
-     */
+
+    /* Read the file char-by-char from the mmap */
      unsigned char *f;
      f = (char *) mmap (0, filesize, PROT_READ, MAP_PRIVATE, dfd, 0);
      for (int i = 0; i < filesize; i++) {
          char c;
-
          c = f[i];
          putchar(c);
      }
 
-
-    /* int c;
-     FILE *file;
-     file = fopen("test.txt", "r");
-     if (file) {
-       while ((c = getc(file)) != EOF)
-        putchar(c);
-        fclose(file);
-      }
-
-    for (i = 1; i < filesize; ++i) {
-        printf("%s\n", i, dest[i]);
-    }*/
     munmap(src, filesize);
     munmap(dest, filesize);
 
